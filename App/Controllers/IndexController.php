ec0ace1943e89b67cd9e8f6bfb80c301341299e4
cangussu12 <leo.tywin@gmail.com>
2022-01-10 17:11:09 -0300

    iniciando usuarios

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controllers;
+
+//os recursos do miniframework
+use MF\Controller\Action;
+use MF\Model\Container;
+
+
+class IndexController extends Action {
+
+	public function index() {
+
+		$this->render('index');
+	}
+
+	public function inscreverse() {
+		$this->render('inscreverse');
+	}
+
+	public function registrar() {
+		echo '<pre>';
+		print_r($_POST);
+		echo '</pre>';
+
+		//receber os dados do formulario
+
+		$usuario = Container::getModel('Usuario');
+
+		$usuario->__set('nome', $_POST['nome']);
+		$usuario->__set('email', $_POST['email']);
+		$usuario->__set('senha', $_POST['senha']);
+
+		echo '<pre>';
+		print_r($usuario);
+		echo '</pre>';
+	}
+}
+
+
+?>
